4e3a8bef77d4c0259afe7ed768fa3cb93130340d
urch <urch@Urchbuntu>
2018-07-26 22:01:44 +1200

    Terrain and fog
    Added support for basic single textured terrain and rendering of fog.

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -12,10 +12,13 @@ import entities.Entity;
 import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
+import objConverter.ModelData;
+import objConverter.OBJFileLoader;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
+import terrains.Terrain;
 import textures.ModelTexture;
 
 public class MainGameLoop {
@@ -25,33 +28,67 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();		
 		Loader loader = new Loader();	
 		
-		RawModel model = OBJLoader.loadObjModel("plantpot", loader);
-		
-		TexturedModel plantModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("plantpotTexture")));
+		ModelData data = OBJFileLoader.loadOBJ("cactus");
+		RawModel model = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), 
+				data.getNormals(), data.getIndices());		
+		TexturedModel cactusModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("cactus")));
 
+		data = OBJFileLoader.loadOBJ("agave");
+		RawModel model2 = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), 
+				data.getNormals(), data.getIndices());		
+		TexturedModel agaveModel = new TexturedModel(model2,new ModelTexture(loader.loadTexture("agave")));
+
+		data = OBJFileLoader.loadOBJ("grass");
+		RawModel model3 = loader.loadToVAO(data.getVertices(), data.getTextureCoords(), 
+				data.getNormals(), data.getIndices());		
+		TexturedModel grassModel = new TexturedModel(model3,new ModelTexture(loader.loadTexture("grass")));
+		grassModel.getTexture().setHasTransparency(true);
+		grassModel.getTexture().setUseFakeLighting(true);
+		
+//		Entity entity = new Entity(plantModel, new Vector3f(0,0,-25),0,0,0,3);
 		Light light = new Light(new Vector3f(3000, 2000, 3000), new Vector3f(1,1,1));
 		
-		Camera camera = new Camera();
-		
-		List<Entity> allPlants = new ArrayList<Entity>();
-		Random random = new Random();
-		
-		for( int i = 0; i < 50000; i++) {
-			float x = random.nextFloat() * 100 - 50;
-			float y = random.nextFloat() * 100 - 50;
-			float z = random.nextFloat() * -300;
-			allPlants.add(new Entity(plantModel, new Vector3f(x,y,z), random.nextFloat() * 180f, 
-					random.nextFloat() * 180f, 0f, 1f));
+//		Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("sand")));
+//		Terrain terrain2 = new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("sand")));
+//		Terrain terrain3 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("sand")));
+		List<Terrain> terrain = new ArrayList<Terrain>();
+		for( int i = 0; i < 4; i++) {
+			terrain.add(new Terrain(i-1,-1,loader,new ModelTexture(loader.loadTexture("sand"))));
 		}
 		
+		Camera camera = new Camera();
 		MasterRenderer renderer = new MasterRenderer();
+		
+		List<Entity> plants = new ArrayList<Entity>();
+		Random random = new Random();		
+		for( int i = 0; i < 500; i++) {
+			plants.add(new Entity(cactusModel, 
+					new Vector3f(random.nextFloat()*800 - 400,0f,random.nextFloat() * -600),
+					0,random.nextFloat()*360,0,2));
+		}
+		for( int i = 0; i < 500; i++) {
+			plants.add(new Entity(agaveModel, 
+					new Vector3f(random.nextFloat()*800 - 400,0f,random.nextFloat() * -600),
+					0,random.nextFloat()*360,0,2));
+		}		
+		for( int i = 0; i < 500; i++) {
+			plants.add(new Entity(grassModel, 
+					new Vector3f(random.nextFloat()*800 - 400,0.5f,random.nextFloat() * -600),
+					0,random.nextFloat()*360,0,2));
+		}			
+		
 		
 		// loop until exit, e.g. x
 		while(!Display.isCloseRequested()) {		
 			camera.move();
-			for(Entity plant : allPlants) {
+			for(Entity plant : plants) {
 				renderer.processEntity(plant);
-				plant.increasePosition(random.nextFloat() * 1f - 0.5f, random.nextFloat() * 1f - 0.5f, random.nextFloat() * 1f - 0.5f);
+			}
+//			renderer.processTerrain(terrain);
+//			renderer.processTerrain(terrain2);
+//			renderer.processTerrain(terrain3);
+			for(Terrain tera : terrain) {
+				renderer.processTerrain(tera);
 			}
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
--- GameEngine/src/renderEngine/EntityRenderer.java
+++ GameEngine/src/renderEngine/EntityRenderer.java
@@ -3,7 +3,6 @@ package renderEngine;
 import java.util.List;
 import java.util.Map;
 
-import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
@@ -17,30 +16,15 @@ import shaders.StaticShader;
 import textures.ModelTexture;
 import toolbox.Maths;
 
-public class Renderer {
+public class EntityRenderer {
 
-	private static final float FOV = 70;
-	private static final float NEAR_PLANE = 0.1f;
-	private static final float FAR_PLANE = 1000;
-	
-	private Matrix4f projectionMatrix;
 	private StaticShader shader;
 	
-	public Renderer(StaticShader shader) {
+	public EntityRenderer(StaticShader shader, Matrix4f projectionMatrix) {
 		this.shader = shader;
-		GL11.glEnable(GL11.GL_CULL_FACE);
-		GL11.glCullFace(GL11.GL_BACK);
-		createProjectionMatrix();
 		shader.start();
 		shader.loadProjectionMatrix(projectionMatrix);
 		shader.stop();
-	}
-	
-	// called once every frame, prepares opengl to render the game
-	public void prepare() {
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);		
-		GL11.glClearColor(0, 0.3f, 0.0f, 1);
 	}
 	
 	public void render(Map<TexturedModel,List<Entity>> entities) {
@@ -63,12 +47,17 @@ public class Renderer {
 		GL20.glEnableVertexAttribArray(1);
 		GL20.glEnableVertexAttribArray(2);		
 		ModelTexture texture = model.getTexture();
+		if(texture.isHasTransparency()) {
+			MasterRenderer.disableCulling();
+		}
+		shader.loadFakeLightingVariable(texture.isUseFakeLighting());
 		shader.loadShineVariables(texture.getShineDamper(), texture.getReflectivity());
 		GL13.glActiveTexture(GL13.GL_TEXTURE0);
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getID());	
 	}
 	
 	private void unbindTexturedModel() {
+		MasterRenderer.enableCulling();
 		GL20.glDisableVertexAttribArray(0);
 		GL20.glDisableVertexAttribArray(1);
 		GL20.glDisableVertexAttribArray(2);
@@ -81,18 +70,4 @@ public class Renderer {
 		shader.loadTransformationMatrix(transformationMatrix);		
 	}
 	
-	private void createProjectionMatrix() {
-		float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
-		float y_scale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
-		float x_scale = y_scale / aspectRatio;
-		float frustum_length = FAR_PLANE - NEAR_PLANE;
-
-		projectionMatrix = new Matrix4f();
-		projectionMatrix.m00 = x_scale;
-		projectionMatrix.m11 = y_scale;
-		projectionMatrix.m22 = -((FAR_PLANE + NEAR_PLANE) / frustum_length);
-		projectionMatrix.m23 = -1;
-		projectionMatrix.m32 = -((2 * NEAR_PLANE * FAR_PLANE) / frustum_length);
-		projectionMatrix.m33 = 0;		
-	}
 }
